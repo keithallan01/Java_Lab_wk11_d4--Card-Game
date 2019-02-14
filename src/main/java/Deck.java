94959486b6fe319eaac42e605fc0eca2bfb843b4
Keith Allan <keithallan01@gmail.com>
2019-02-14 14:10:54 +0000

    create Deck class with cardList ArrayList

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cardList;
+
+    public Deck(){
+        cardList = new ArrayList<>();
+    }
+
+    public int cardCount(){
+        return this.cardList.size();
+    }
+
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void before(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void hasEmptyListOfCards(){
+        assertEquals(0, deck.cardCount());
+    }
 }
