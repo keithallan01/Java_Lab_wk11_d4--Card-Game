7e14da194c3eb3115a19a6b8a988d9ac02afb9fa
Keith Allan <keithallan01@gmail.com>
2019-02-14 15:38:56 +0000

    game can find winner

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,32 @@
 public class Game {
+    private Player player1;
+    private Player player2;
+    private Deck deck;
+
+    public Game(Player player1, Player player2){
+        deck = new Deck();
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player findWinner(){
+        int player1result = this.player1.showCard().getRank().getValue();
+        int player2result = this.player2.showCard().getRank().getValue();
+
+        if (player1result > player2result) {
+            return player1;
+        }
+        else if (player2result > player1result) {
+            return player2;
+        } else {
+            return null;
+        }
+    }
+
+    public String itsADraw(){
+        return "It's a Draw";
+    }
+
+
+
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,8 +1,33 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
-//    Game game;
-//
-//    @Before
+    Game game;
+    Player player1;
+    Player player2;
+    Card card1;
+    Card card2;
+
+    @Before
+    public void before(){
+
+        player1 = new Player();
+        player2 = new Player();
+        game = new Game(player1, player2);
+        card1 = new Card(SuitType.CLUBS, RankType.EIGHT);
+        card2 = new Card(SuitType.SPADES, RankType.QUEEN);
+    }
+
+    @Test
+    public void canDetermineWinner(){
+        player1.takeCard(card1);
+        player2.takeCard(card2);
+        assertEquals(player2, game.findWinner());
+    }
+
+
+
 }
