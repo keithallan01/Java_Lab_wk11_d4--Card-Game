6745c2ad63194e683276f5d0adf27a1750c6bf12
Keith Allan <keithallan01@gmail.com>
2019-02-14 13:54:39 +0000

    initial commit, classes and enums created

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest {
+
+    Card card;
+
+    @Before
+    public void before(){
+        card = new Card("Hearts", "five");
+    }
+
+    @Test
+    public void hasSuit(){
+        assertEquals("Hearts", card.getSuit());
+    }
+
+}
